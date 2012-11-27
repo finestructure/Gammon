0a38ff0c44d25dd2fe87d8656f19277762ca837e
Sven A. Schmidt <sas@abstracture.de>
2012-11-27 19:58:20 +0000

    Added background pattern and colour definitions

---
+++ Gammon/UIColor+Gammon.h
@@ -0,0 +1,17 @@
+//
+//  UIColor+Gammon.h
+//  Gammon
+//
+//  Created by Sven A. Schmidt on 27.11.12.
+//  Copyright (c) 2012 Sven A. Schmidt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Gammon)
+
++ (UIColor *)mossColor;
++ (UIColor *)pipColor1;
++ (UIColor *)pipColor2;
+
+@end
---
+++ Gammon/UIColor+Gammon.m
@@ -0,0 +1,28 @@
+//
+//  UIColor+Gammon.m
+//  Gammon
+//
+//  Created by Sven A. Schmidt on 27.11.12.
+//  Copyright (c) 2012 Sven A. Schmidt. All rights reserved.
+//
+
+#import "UIColor+Gammon.h"
+
+@implementation UIColor (Gammon)
+
++ (UIColor *)mossColor
+{
+  return [UIColor colorWithRed:0.000 green:0.502 blue:0.251 alpha:1.000];
+}
+
++ (UIColor *)pipColor1
+{
+  return [UIColor colorWithRed:208.0/255.0 green:208.0/255.0 blue:208.0/255.0 alpha:1.0];
+}
+
++ (UIColor *)pipColor2
+{
+  return [UIColor colorWithRed:0.600 green:0.400 blue:0.200 alpha:1.000];
+}
+
+@end
